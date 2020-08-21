d6d926e69f5bf3ad89170426257c923cf8d52d45
Mikhail Ostronosov <supermanmgdn@gmail.com>
2020-08-21 03:05:59 +0300

    Added background and changed Rolling Nero a little bit

--- Assets/Scripts/Rolling Nero/NeroColliderListener.cs
+++ Assets/Scripts/Rolling Nero/NeroColliderListener.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 public class NeroColliderListener : MonoBehaviour
 {
     public PlayerHealth playerHealth;
+    private RollingNeroMovement rollingNeroMovement;
+    private Rigidbody2D rigidBody2D;
+    private Animator animator;
     public string currentTag;//Тэг сработавшего коллайдера
     
 
     //Подключение коллайдеров детей
     private void Awake()
     {
+        rollingNeroMovement = GetComponent<RollingNeroMovement>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
         var colliders = GetComponentsInChildren<Collider2D>();
         foreach (var col in colliders) 
         {
@@ -41,6 +48,15 @@ public class NeroColliderListener : MonoBehaviour
                 playerHealth.Damage();
             }
         }
+        else if (collision.gameObject.CompareTag("Border"))
+        {
+            if (currentTag == "Body")
+            {
+                Debug.Log("Body Collider");
+                rollingNeroMovement.stopped = true;
+
+            }
+        }
         
     }
 
@@ -53,6 +69,8 @@ public class NeroColliderListener : MonoBehaviour
         yield return new WaitForSeconds(5);
         Destroy(gameObject);
     }
+
+    
 
 
 
