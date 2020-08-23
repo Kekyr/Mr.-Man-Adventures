6e4f32d6deba09b23c8af58265a1139298cdb07d
Mikhail Ostronosov <supermanmgdn@gmail.com>
2020-08-23 04:59:00 +0300

    Added new enemy Octi

---
+++ Assets/Scripts/Octi/OctiColliderBridge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class OctiColliderBridge : MonoBehaviour
+{
+    private OctiColliderListener _listener; //Родитель всех gameobject
+    public void Initialize(OctiColliderListener l)
+    {
+        _listener = l;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _listener.currentTag = this.gameObject.tag;
+        _listener.OnCollisionEnter2D(collision);
+    }
+}
--- Assets/Scripts/Rolling Nero/RollingNeroMovement.cs
+++ Assets/Scripts/Rolling Nero/RollingNeroMovement.cs
@@ -12,23 +12,14 @@ public class RollingNeroMovement : MonoBehaviour
     public bool stopped = false;
     private bool facingRight = false;//Направление спрайта
     private Vector3 velocity = Vector3.zero;
-    private Vector3 startPosition;
-    private Vector3 firstDestination;
-    private Vector3 secondDestination;
-    private Vector3 currentDestination;
     public Vector3 targetVelocity;
 
-   
 
     private void Awake()
     {
         common = FindObjectOfType<Common>();
         animator = GetComponent<Animator>();
         rigidBody2D = GetComponent<Rigidbody2D>();
-        startPosition = this.transform.position + new Vector3(0.5f, 0);
-        firstDestination = startPosition + new Vector3(5.3f, 0);
-        secondDestination = startPosition + new Vector3(10f, 0);
-        currentDestination = firstDestination;
     }
 
     private void Start()
@@ -41,70 +32,38 @@ public class RollingNeroMovement : MonoBehaviour
     {
         while (true)
         {
-                if (!enabled)
-                {
-                    yield break;
-                }
+            if (!enabled)
+            {
+                yield break;
+            }
 
-                if(stopped)
-                {
-                  rigidBody2D.velocity = Vector3.zero;
-                  rollSpeed *= -1;
-                  animator.SetFloat("Speed", Mathf.Abs(rigidBody2D.velocity.x));
-                  yield return new WaitForSeconds(5);
-                  stopped = false;
-                }
-
-                targetVelocity = new Vector2(rollSpeed, rigidBody2D.velocity.y);
-
-                rigidBody2D.velocity = Vector3.SmoothDamp(rigidBody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
-
-
-                //if (rigidBody2D.position.x > currentDestination.x && rollSpeed > 0)
-                //{
-                //    rigidBody2D.velocity = Vector3.zero;
-                //    animator.SetFloat("Speed", Mathf.Abs(rigidBody2D.velocity.x));
-                //    if (currentDestination == firstDestination)
-                //    {
-                //        currentDestination = secondDestination;
-                //    }
-                //    else if (currentDestination == secondDestination)
-                //    {
-                //        currentDestination = firstDestination;
-                //        rollSpeed *= -1;
-                //    }
-                //    yield return new WaitForSeconds(5);
-                //}
-                //else if (rigidBody2D.position.x < currentDestination.x && rollSpeed < 0)
-                //{
-                //    rigidBody2D.velocity = Vector3.zero;
-                //    animator.SetFloat("Speed", Mathf.Abs(rigidBody2D.velocity.x));
-                //    if (currentDestination == firstDestination)
-                //    {
-                //        currentDestination = startPosition;
-                //    }
-                //    else if (currentDestination == startPosition)
-                //    {
-                //        currentDestination = firstDestination;
-                //        rollSpeed *= -1;
-                //    }
-                //    yield return new WaitForSeconds(5);
-                //}
-
-                if (rollSpeed > 0 && !facingRight)
-                {
-                    common.Flip(ref facingRight, gameObject);
-                }
-
-                else if (rollSpeed < 0 && facingRight)
-                {
-                    common.Flip(ref facingRight, gameObject);
-                }
-
+            if (stopped)
+            {
+                rigidBody2D.velocity = Vector3.zero;
+                rollSpeed *= -1;
                 animator.SetFloat("Speed", Mathf.Abs(rigidBody2D.velocity.x));
+                yield return new WaitForSeconds(5);
+                stopped = false;
+            }
 
-                yield return null;
-            
+            targetVelocity = new Vector2(rollSpeed, rigidBody2D.velocity.y);
+
+            rigidBody2D.velocity = Vector3.SmoothDamp(rigidBody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+            if (rollSpeed > 0 && !facingRight)
+            {
+                common.Flip(ref facingRight, gameObject);
+            }
+
+            else if (rollSpeed < 0 && facingRight)
+            {
+                common.Flip(ref facingRight, gameObject);
+            }
+
+            animator.SetFloat("Speed", Mathf.Abs(rigidBody2D.velocity.x));
+
+            yield return null;
+
 
         }
     }
