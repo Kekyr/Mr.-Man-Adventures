e2b0927990d1630e32a40454f00a48f450eda486
Mikhail Ostronosov <supermanmgdn@gmail.com>
2020-10-19 21:28:40 +0300

    Added moving camera and few tiles

---
+++ Assets/Scripts/Other/EndOfTheLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EndOfTheLevel : MonoBehaviour
+{
+    public MovingCamera movingCamera;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        movingCamera.enabled = false;
+    }
+}
